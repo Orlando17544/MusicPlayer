e53aebee5bd3beff0e31dc1269ffaca0510cafd5
Orlando Canchola Jimenez <orcj17544@gmail.com>
2022-11-27 10:56:09 -0600

    Now the playlists can be added

--- app/src/main/java/com/example/android/youtubemusicplayer/playlists/PlaylistsFragment.kt
+++ app/src/main/java/com/example/android/youtubemusicplayer/playlists/PlaylistsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.android.youtubemusicplayer.playlists
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.youtubemusicplayer.R
 import com.example.android.youtubemusicplayer.database.MusicDatabase
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class PlaylistFragment : Fragment() {
 
@@ -47,6 +53,40 @@ class PlaylistFragment : Fragment() {
         recyclerView.adapter = adapter;
 
         viewModel = ViewModelProvider(this).get(PlaylistsViewModel::class.java)
+
+        val addPlaylist = view.findViewById<ImageView>(R.id.add_playlist);
+
+        addPlaylist.setOnClickListener(View.OnClickListener {
+
+            // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+            val builder: AlertDialog.Builder? = activity?.let {
+                val builder = AlertDialog.Builder(it);
+
+                val inflater = requireActivity().layoutInflater;
+
+                val addPlaylistView = inflater.inflate(R.layout.add_playlist, null);
+
+                builder.apply {
+                    setView(addPlaylistView)
+                    setPositiveButton("Ok",
+                        DialogInterface.OnClickListener { dialog, id ->
+                            // User clicked OK button
+
+                            val newPlaylistEditText = addPlaylistView.findViewById<TextInputEditText>(R.id.new_playlist_name);
+                            viewModel.addPlaylist(newPlaylistEditText.text.toString());
+                        })
+                    setNegativeButton("Cancel",
+                        DialogInterface.OnClickListener { dialog, id ->
+                            // User cancelled the dialog
+                        })
+                }
+            }
+
+            // 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
+            val dialog: AlertDialog? = builder?.create();
+
+            dialog?.show();
+        })
 
         return view;
     }
--- app/src/main/java/com/example/android/youtubemusicplayer/playlists/PlaylistsViewModel.kt
+++ app/src/main/java/com/example/android/youtubemusicplayer/playlists/PlaylistsViewModel.kt
@@ -4,12 +4,23 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.youtubemusicplayer.database.Playlist
 import com.example.android.youtubemusicplayer.database.PlaylistWithSongs
 import com.example.android.youtubemusicplayer.database.Song
 import com.example.android.youtubemusicplayer.database.MusicDatabaseDao
+import kotlinx.coroutines.launch
 
 class PlaylistsViewModel(val database: MusicDatabaseDao,
                          application: Application) : AndroidViewModel(application) {
     val playlistWithSongs : LiveData<List<PlaylistWithSongs>> = database.getPlaylistsWithSongs();
+
+    fun addPlaylist(playlistName: String) {
+        val newPlaylist = Playlist();
+        newPlaylist.name = playlistName;
+
+        viewModelScope.launch {
+            database.insertPlaylist(newPlaylist);
+        }
+    }
 }
